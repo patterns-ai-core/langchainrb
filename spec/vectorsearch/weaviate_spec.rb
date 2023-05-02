f70a4e335210c6cf2f1a0f265d8acd780f9f50d7
Andrei Bondarev <andrei.bondarev13@gmail.com>
2023-05-02 16:54:25 -0500

    Additional Weaviate specs

--- spec/vectorsearch/weaviate_spec.rb
+++ spec/vectorsearch/weaviate_spec.rb
@@ -23,11 +23,69 @@ RSpec.describe Vectorsearch::Weaviate do
     end
   end
 
-  xdescribe "#add_texts"
+  describe "#add_texts" do
+    let(:fixture) { JSON.parse(File.read("spec/fixtures/vectorsearch/weaviate_add_texts.json")) }
 
-  xdescribe "#similarity_search"
+    before do
+      allow_any_instance_of(
+        Weaviate::Objects
+      ).to receive(:batch_create)
+      .with(
+        objects: [{
+          class: "products",
+          properties: { content: "Hello World" }
+        }]
+      )
+      .and_return(fixture)
+    end
 
-  xdescribe "#similarity_search_by_vector"
+    it "adds texts" do
+      expect(subject.add_texts(texts: ["Hello World"])).to eq(fixture)
+    end
+
+  end
+
+  describe "#similarity_search" do
+    let(:fixture) { JSON.parse(File.read("spec/fixtures/vectorsearch/weaviate_search.json")) }
+
+    before do
+      allow_any_instance_of(
+        Weaviate::Query
+      ).to receive(:get)
+      .with(
+        class_name: "products",
+        near_text: "{ concepts: [\"earth\"] }",
+        limit: "4",
+        fields: "content _additional { id }"
+      )
+      .and_return(fixture)
+    end
+
+    it "searches for similar texts" do
+      expect(subject.similarity_search(query: "earth")).to eq(fixture)
+    end
+  end
+
+  describe "#similarity_search_by_vector" do
+    let(:fixture) { JSON.parse(File.read("spec/fixtures/vectorsearch/weaviate_search.json")) }
+
+    before do
+      allow_any_instance_of(
+        Weaviate::Query
+      ).to receive(:get)
+      .with(
+        class_name: "products",
+        near_vector: "{ vector: [0.1, 0.2, 0.3] }",
+        limit: "4",
+        fields: "content _additional { id }"
+      )
+      .and_return(fixture)
+    end
+
+    it "searches for similar vectors" do
+      expect(subject.similarity_search_by_vector(embedding: [0.1, 0.2, 0.3])).to eq(fixture)
+    end
+  end
 
   xdescribe "#ask"
 end
