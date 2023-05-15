e6d2171d9e3dda825a93a6c9b985e0e6044e1e46
Ricky Chilcott <ricky@missionmet.com>
2023-05-15 09:01:25 -0400

    Return true if we successfully have depended on a gem

--- lib/optional_dependency_helper.rb
+++ lib/optional_dependency_helper.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
-def depends_on(name)
-  gem name # require the gem
+def depends_on(gem_name)
+  gem(gem_name) # require the gem
 
-  return unless defined?(Bundler) # If we're in a non-bundler environment, we're no longer able to determine if we'll meet requirements
+  return(true) unless defined?(Bundler) # If we're in a non-bundler environment, we're no longer able to determine if we'll meet requirements
 
-  gem_version = Gem.loaded_specs[name].version
-  gem_requirement = Bundler.load.dependencies.find { |g| g.name == name }.requirement
+  gem_version = Gem.loaded_specs[gem_name].version
+  gem_requirement = Bundler.load.dependencies.find { |g| g.name == gem_name }.requirement
 
   if !gem_requirement.satisfied_by?(gem_version)
-    raise "The #{name} gem is installed, but version #{gem_requirement} is required. You have #{gem_version}."
+    raise "The #{gem_name} gem is installed, but version #{gem_requirement} is required. You have #{gem_version}."
   end
+
+  true
 rescue LoadError
-  raise LoadError, "Could not load #{name}. Please ensure that the #{name} gem is installed."
+  raise LoadError, "Could not load #{gem_name}. Please ensure that the #{gem_name} gem is installed."
 end
