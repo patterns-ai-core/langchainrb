c90a7321fdc99e415d9e4078b4af5f66b8217ca8
Andrei Bondarev <andrei.bondarev13@gmail.com>
2023-05-20 14:39:33 -0500

    spec

--- spec/llm/replicate_spec.rb
+++ spec/llm/replicate_spec.rb
@@ -28,4 +28,20 @@ RSpec.describe LLM::Replicate do
       expect(subject.send(:embeddings_model)).to be_a(Replicate::Record::ModelVersion)
     end
   end
+
+  describe "#embed" do
+    let(:embeddings_model) { Replicate::Record::ModelVersion.new({}, {}) }
+    let(:embedding) { [0.1, 0.2, 0.3] }
+
+    before do
+      allow(subject.client).to receive_message_chain(:retrieve_model, :latest_version).and_return(embeddings_model)
+      allow(embeddings_model).to predict(input: text).and_return(
+        double(finished?: true, output: embedding)
+      )
+    end
+
+    it "returns an embedding" do
+      expect(subject.embed(text: "Hello World")).to eq(embedding)
+    end
+  end
 end
