bddf45b5e3ae2705422e97bc5e6239e752909f81
Ricky Chilcott <ricky@missionmet.com>
2023-05-21 21:56:40 -0400

    Chunking

--- lib/chunkers/base.rb
+++ lib/chunkers/base.rb
@@ -7,7 +7,7 @@ module Chunkers
     LENGTH_FUNCTION = ->(text) { text.length }
 
     def initialize(chunk_size: 4000, chunk_overlap: 0, length: LENGTH_FUNCTION, **kwargs)
-      raise ArgumentError, "Got a larger chunk overlap (#{chunk_overlap}) than chunk size (#{chunk_size}), should be smaller." if chunk_overlap > chunk_size
+      raise ArgumentError, "Got a larger chunk overlap (#{chunk_overlap}) than chunk size (#{chunk_size}), should be smaller." if chunk_overlap >= chunk_size
 
       @chunk_size = chunk_size
       @chunk_overlap = chunk_overlap
@@ -16,6 +16,28 @@ module Chunkers
 
     def split_text(text)
       raise NotImplementedError
+    end
+
+    def chunked(text)
+      split_text(text)
+        .then { |a| split_into_chunks(a) }
+        .then { |a| overlap_chunks(a) }
+    end
+
+    private
+
+    def split_into_chunks(chunks)
+      chunks
+        .map { |chunk| chunk.scan(/.{1,#{chunk_size}}/) }
+        .flatten
+    end
+
+    def overlap_chunks(chunks)
+      return chunks if chunk_overlap.zero?
+
+      # TODO - get this working
+      # chunks
+      #   ...
     end
   end
 end
--- spec/chunkers/base_spec.rb
+++ spec/chunkers/base_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe Chunkers::Base do
   describe "#initialize" do
-    context "when chunk_overlap is greater than chunk_size" do
+    context "when chunk_overlap is greater than or equal chunk_size" do
       it "raises an error" do
         expect { described_class.new(chunk_size: 200, chunk_overlap: 400) }.to raise_error(ArgumentError)
       end
@@ -18,6 +18,40 @@ RSpec.describe Chunkers::Base do
   describe "#split_text" do
     it "raises NotImplementedError" do
       expect { subject.split_text("test") }.to raise_error(NotImplementedError)
+    end
+  end
+
+  describe "#chunked" do
+    it "splits bigger chunks" do
+      text = "a" * 1000
+      subject = described_class.new(chunk_size: 100)
+
+      def subject.split_text(text)
+        Array.new(1, text)
+      end
+
+      expect(subject.chunked(text).size).to eq(10)
+    end
+
+    xit "splits with overlap" do
+      text = "this" + "that" + "thee"
+      subject = described_class.new(chunk_size: 3, chunk_overlap: 2)
+
+      def subject.split_text(text)
+        Array.new(1, text)
+      end
+
+      chunked_overlapped_text = subject.chunked(text)
+      expect(chunked_overlapped_text[0]).to eq("thi")
+      expect(chunked_overlapped_text[1]).to eq("his")
+      expect(chunked_overlapped_text[2]).to eq("ist")
+      expect(chunked_overlapped_text[2]).to eq("sth")
+      expect(chunked_overlapped_text[2]).to eq("tha")
+      expect(chunked_overlapped_text[2]).to eq("hat")
+      expect(chunked_overlapped_text[2]).to eq("att")
+      expect(chunked_overlapped_text[2]).to eq("tte")
+      expect(chunked_overlapped_text[2]).to eq("tee")
+      expect(chunked_overlapped_text[2]).to eq("ee")
     end
   end
 end
