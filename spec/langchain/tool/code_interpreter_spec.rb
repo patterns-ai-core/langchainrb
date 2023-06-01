ac275257da6f017921aa32fd412d6652d9cae857
Bruno Bornsztein <bruno.bornsztein@gmail.com>
2023-05-31 21:35:22 -0500

    add another test

--- spec/langchain/tool/code_interpreter_spec.rb
+++ spec/langchain/tool/code_interpreter_spec.rb
@@ -5,5 +5,17 @@ RSpec.describe Tool::CodeInterpreter do
     it "executes the expression" do
       expect(subject.execute(input: '"hello world".reverse!')).to eq("dlrow olleh")
     end
+
+    it "executes a more complicated expression" do
+      code = <<~CODE
+        def reverse(string)
+          string.reverse!
+        end
+
+        reverse('hello world')
+      CODE
+
+      expect(subject.execute(input: code)).to eq("dlrow olleh")
+    end
   end
 end
