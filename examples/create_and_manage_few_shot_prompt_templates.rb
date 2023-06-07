327e8a1f9824360e6bcf52ca1554be547eb68cd2
Andrei Bondarev <andrei@sourcelabs.io>
2023-06-07 16:11:45 -0500

    Fix examples/ (#146)

--- examples/create_and_manage_few_shot_prompt_templates.rb
+++ examples/create_and_manage_few_shot_prompt_templates.rb
@@ -1,10 +1,10 @@
 require "langchain"
 
 # Create a prompt with a few shot examples
-prompt = Prompt::FewShotPromptTemplate.new(
+prompt = Langchain::Prompt::FewShotPromptTemplate.new(
   prefix: "Write antonyms for the following words.",
   suffix: "Input: {adjective}\nOutput:",
-  example_prompt: Prompt::PromptTemplate.new(
+  example_prompt: Langchain::Prompt::PromptTemplate.new(
     input_variables: ["input", "output"],
     template: "Input: {input}\nOutput: {output}"
   ),
@@ -32,5 +32,5 @@ prompt.format(adjective: "good")
 prompt.save(file_path: "spec/fixtures/prompt/few_shot_prompt_template.json")
 
 # Loading a new prompt template using a JSON file
-prompt = Prompt.load_from_path(file_path: "spec/fixtures/prompt/few_shot_prompt_template.json")
+prompt = Langchain::Prompt.load_from_path(file_path: "spec/fixtures/prompt/few_shot_prompt_template.json")
 prompt.prefix # "Write antonyms for the following words."
