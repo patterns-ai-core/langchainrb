844bfb6e184247e33965ab330f2a75cde020e4d2
Andrei Bondarev <andrei.bondarev13@gmail.com>
2023-06-07 19:22:40 -0500

    Refactor

--- spec/langchain/agent/chain_of_thought_agent/chain_of_thought_agent_spec.rb
+++ spec/langchain/agent/chain_of_thought_agent/chain_of_thought_agent_spec.rb
@@ -20,58 +20,57 @@ RSpec.describe Langchain::Agent::ChainOfThoughtAgent do
   describe "#run" do
     let(:question) { "What is the square root of the average temperature in Miami, Florida in May?" }
 
-    let(:original_prompt) {
+    let(:first_prompt) {
       subject.send(:create_prompt,
         question: question,
         tools: subject.tools)
     }
-    let(:updated_prompt) { original_prompt + llm_first_response + "\nObservation: #{search_tool_response}\nThought:" }
-    let(:updated_prompt_2) { updated_prompt + llm_second_response + "\nObservation: #{calculator_tool_response}\nThought:" }
-    let(:final_prompt) { updated_prompt_2 + llm_third_response + "\nObservation: #{calculator_tool_response_2}\nThought:" }
+    let(:second_prompt) { first_prompt + first_response + "\nObservation: #{search_response}\nThought:" }
+    let(:third_prompt) { second_prompt + second_response + "\nObservation: #{calculator_response}\nThought:" }
+    let(:final_prompt) { third_prompt + third_response + "\nObservation: #{calculator_response_2}\nThought:" }
 
-    let(:llm_first_response) { " I need to find the average temperature in Miami, Florida in May and then calculate the square root of that number.\nAction: search\nAction Input: average temperature in Miami, Florida in May" }
-    let(:search_tool_response) { "May Weather in Miami Florida, United States. Daily high temperatures increase by 3°F, from 83°F to 86°F, rarely falling below 79°F or exceeding 90° ..." }
-    let(:llm_second_response) { " I need to calculate the average temperature\nAction: calculator\nAction Input: (83+86+79+90)/4\n\n" }
-    let(:calculator_tool_response) { "84.5" }
-    let(:llm_third_response) { " I now have the average temperature and can calculate the square root\nAction: calculator\nAction Input: sqrt(84.5)\n\n" }
-    let(:calculator_tool_response_2) { "9.192388155425117" }
+    let(:first_response) { " I need to find the average temperature in Miami, Florida in May and then calculate the square root of that number.\nAction: search\nAction Input: average temperature in Miami, Florida in May" }
+    let(:search_response) { "May Weather in Miami Florida, United States. Daily high temperatures increase by 3°F, from 83°F to 86°F, rarely falling below 79°F or exceeding 90° ..." }
+    let(:second_response) { " I need to calculate the average temperature\nAction: calculator\nAction Input: (83+86+79+90)/4\n\n" }
+    let(:calculator_response) { "84.5" }
+    let(:third_response) { " I now have the average temperature and can calculate the square root\nAction: calculator\nAction Input: sqrt(84.5)\n\n" }
+    let(:calculator_response_2) { "9.192388155425117" }
 
     let(:final_answer) { "9.2" }
-
-    let(:llm_final_response) { " I now know the final answer\nFinal Answer: #{final_answer}" }
+    let(:final_response) { " I now know the final answer\nFinal Answer: #{final_answer}" }
 
     before do
       allow(subject.llm).to receive(:complete).with(
-        prompt: original_prompt,
+        prompt: first_prompt,
         stop_sequences: ["Observation:"]
-      ).and_return(llm_first_response)
+      ).and_return(first_response)
 
       allow(subject.tools[1]).to receive(:execute).with(
         input: "average temperature in Miami, Florida in May"
-      ).and_return(search_tool_response)
+      ).and_return(search_response)
 
       allow(subject.llm).to receive(:complete).with(
-        prompt: updated_prompt,
+        prompt: second_prompt,
         stop_sequences: ["Observation:"]
-      ).and_return(llm_second_response)
+      ).and_return(second_response)
 
       allow(subject.tools[0]).to receive(:execute).with(
         input: "(83+86+79+90)/4"
-      ).and_return(calculator_tool_response)
+      ).and_return(calculator_response)
 
       allow(subject.llm).to receive(:complete).with(
-        prompt: updated_prompt_2,
+        prompt: third_prompt,
         stop_sequences: ["Observation:"]
-      ).and_return(llm_third_response)
+      ).and_return(third_response)
 
       allow(subject.tools[0]).to receive(:execute).with(
         input: "sqrt(84.5)"
-      ).and_return(calculator_tool_response_2)
+      ).and_return(calculator_response_2)
 
       allow(subject.llm).to receive(:complete).with(
         prompt: final_prompt,
         stop_sequences: ["Observation:"]
-      ).and_return(llm_final_response)
+      ).and_return(final_response)
     end
 
     it "runs the agent" do
