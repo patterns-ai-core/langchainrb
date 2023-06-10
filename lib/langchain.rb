c6ee797811bbc4a952e039c469f9cee44f08ac34
Matt Lindsey <matthew.r.lindsey@gmail.com>
2023-06-10 12:52:50 -0400

    Update serch.rb filename

--- lib/langchain.rb
+++ lib/langchain.rb
@@ -74,7 +74,7 @@ module Langchain
     autoload :Base, "langchain/tool/base"
     autoload :Calculator, "langchain/tool/calculator"
     autoload :RubyCodeInterpreter, "langchain/tool/ruby_code_interpreter"
-    autoload :Search, "langchain/tool/serp_api"
+    autoload :Search, "langchain/tool/search"
     autoload :Wikipedia, "langchain/tool/wikipedia"
     autoload :Database, "langchain/tool/database"
   end
