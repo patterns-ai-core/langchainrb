973b93659526d950942ac81d8fdacd9e15bc42a0
Matt Lindsey <matthew.r.lindsey@gmail.com>
2023-06-10 06:31:25 -0400

    Support current weather

--- lib/langchain/tool/weather.rb
+++ lib/langchain/tool/weather.rb
@@ -7,6 +7,7 @@ module Langchain::Tool
     #
     # Gem requirements:
     #   gem "open-weather-ruby-client", "~> 0.3.0"
+    #   api_key: https://home.openweathermap.org/api_keys
     #
 
     NAME = "weather"
@@ -14,12 +15,15 @@ module Langchain::Tool
     description <<~DESC
       Useful for getting current, forecast, or historical weather data
   
-      The input to this tool should be a city name followed by the type of weather you want to get.
+      The input to this tool should be a city name followed by the type of weather (current or forecast) you want to get, followed optionally by the units (imperial, metric, or standard)
+      Example usage:
+        Action Input: Boston, current, imperial
+        Weather: The current weather in Boston is temp 52.47, feels_like 51.78, temp_min 48.56, temp_max 55.22, pressure 1006, humidity 93
     DESC
 
-    attr_reader :client
+    attr_reader :client, :units
 
-    def initialize(api_key:)
+    def initialize(api_key:, units: "metric")
       depends_on "open-weather-ruby-client"
       require "open-weather-ruby-client"
 
@@ -32,18 +36,28 @@ module Langchain::Tool
     end
 
     # Returns weather for a city
-    # @param input [String] city + type of weather (current, forecast or historical)
+    # @param input [String] city + ',' + type of weather (current, forecast or historical) + unit (optional: imperial, metric, or standard)
     # @return [String] Answer
     def execute(input:)
       Langchain.logger.info("[#{self.class.name}]".light_blue + ": Executing for \"#{input}\"")
 
-      type = input.split(", ").last
+      if input.count(",") > 1
+        units = input.split(", ").last
+        type = input.split(", ")[0..-2][1]
+      else
+        type = input.split(", ").last
+      end
 
       if type === "current"
-        data = client.current_weather(city: input.split(", ").first)
-        "The current weather in #{data.name} is #{data.main.temp} degrees Farenheit"
+        data = client.current_weather(city: input.split(", ").first, units: units)
+        weather = data.main.map { |key, value| "#{key} #{value}" }.join(", ")
+        "The current weather in #{data.name} is #{weather}"
+      elsif type === "forecast"
+        # TODO: data = client.one_call(lat: 33.441792, lon: -94.037689) # => OpenWeather::Models::OneCall::Weather
+        "forcasts coming soon from this tool"
       else
-        "#{type} not yet implemented"
+        # TODO: Do we support this? It's only available for paid OpenWeather accounts.
+        "#{type} not yet implemented by this tool"
       end
     end
   end
