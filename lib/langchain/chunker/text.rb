2cabd2210969efa2bf8db0f2f6ddbe2c2003de14
Andrei Bondarev <andrei.bondarev13@gmail.com>
2023-06-18 12:34:10 -0500

    Code comments

--- lib/langchain/chunker/text.rb
+++ lib/langchain/chunker/text.rb
@@ -4,9 +4,19 @@ require "baran"
 
 module Langchain
   module Chunker
+    #
+    # Simple text chunker
+    #
+    # Usage:
+    #     Langchain::Chunker::Text.new(text).chunks
+    #
     class Text < Base
       attr_reader :text, :chunk_size, :chunk_overlap, :separator
 
+      # @param [String] text
+      # @param [Integer] chunk_size
+      # @param [Integer] chunk_overlap
+      # @param [String] separator
       def initialize(text, chunk_size: 1000, chunk_overlap: 200, separator: "\n\n")
         @text = text
         @chunk_size = chunk_size
@@ -14,6 +24,7 @@ module Langchain
         @separator = separator
       end
 
+      # @return [Array<String>]
       def chunks
         splitter = Baran::CharacterTextSplitter.new(
           chunk_size: chunk_size,
