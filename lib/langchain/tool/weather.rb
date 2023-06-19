3eec271ab00ecc8a7798c7ce9e2ba987cbf71e31
Matt Lindsey <matthew.r.lindsey@gmail.com>
2023-06-19 06:45:33 -0400

    Fix for nonexistant cities

--- lib/langchain/tool/weather.rb
+++ lib/langchain/tool/weather.rb
@@ -22,7 +22,9 @@ module Langchain::Tool
     description <<~DESC
       Useful for getting current weather data
 
-      The input to this tool should be a city name followed by the units (imperial, metric, or standard)
+      The input to this tool should be a city name followed by the units (imperial, metric, or standard).
+      The Observation temperature will be in Fahrenheit, Celsius, or Kelvin respectively.
+
       Usage:
         Action Input: St Louis, Missouri; metric
         Action Input: Boston, Massachusetts; imperial
@@ -61,17 +63,22 @@ module Langchain::Tool
       begin
         data = @client.current_weather(city: city, units: units)
       rescue Faraday::ResourceNotFound
-        input_array = city.split(",")
-        city, state, country = *input_array.map(&:strip)
-        country = "US" if country.nil?
-        units = "standard"
+        begin
+          # Call to current_weather sometimes fails (e.g, 'Boston, MA'), so try current_city instead
+          input_array = city.split(",")
+          city, state, country = *input_array.map(&:strip)
+          country = "US" if country.nil?
+          units = "standard"
 
-        # Note: only standard units are currently supported by this method and country is required
-        data = @client.current_city(city, state, country)
+          # Only standard units are currently supported by current_city and both state and country are required
+          data = @client.current_city(city, state, country)
+        rescue
+          return "Sorry, I couldn't find the weather for #{city}"
+        end
       end
 
       weather = data.main.map { |key, value| "#{key} #{value}" }.join(", ")
-      "The current weather in #{units} units in #{data.name} is #{weather}"
+      "The current weather in #{data.name} in #{units} units is #{weather}"
     end
   end
 end
