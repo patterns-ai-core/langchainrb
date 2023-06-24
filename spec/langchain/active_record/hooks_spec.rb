0b7de2e8422b6b4c4a8b76b31b27ec4495d9b850
Andrei Bondarev <andrei.bondarev13@gmail.com>
2023-06-24 16:40:10 -0500

    fix

--- spec/langchain/active_record/hooks_spec.rb
+++ spec/langchain/active_record/hooks_spec.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
-RSpec.describe Langchain::ActiveRecord::Hooks do
-  let(:dummy_class) do
-    class Dummy
-      include Langchain::ActiveRecord::Hooks
-    end
-  end
+class Dummy
+  include Langchain::ActiveRecord::Hooks
+end
 
+RSpec.describe Langchain::ActiveRecord::Hooks do
   it "responds to instance methods" do
-    expect(dummy_class.new).to respond_to(:upsert_to_vectorsearch)
-    expect(dummy_class.new).to respond_to(:as_vector)
+    expect(Dummy.new).to respond_to(:upsert_to_vectorsearch)
+    expect(Dummy.new).to respond_to(:as_vector)
   end
 
   it "responds to class methods" do
-    expect(dummy_class).to respond_to(:vectorsearch)
-    expect(dummy_class).to respond_to(:similarity_search)
+    expect(Dummy).to respond_to(:vectorsearch)
+    expect(Dummy).to respond_to(:similarity_search)
   end
 end
