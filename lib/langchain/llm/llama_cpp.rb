8ee23f545fb131d55d9641a825279dd0b839e20c
Josh Nichols <josh.nichols@gusto.com>
2023-07-02 11:39:07 -0400

    include seeds. try to control lifecycle of various llama.cpp to avoid having to allocate unless necessary

--- lib/langchain/llm/llama_cpp.rb
+++ lib/langchain/llm/llama_cpp.rb
@@ -2,10 +2,10 @@
 
 module Langchain::LLM
   class LlamaCpp < Base
-    attr_accessor :model_path, :n_gpu_layers, :n_ctx
+    attr_accessor :model_path, :n_gpu_layers, :n_ctx, :seed
     attr_writer :n_threads
 
-    def initialize(model_path:, n_gpu_layers: 1, n_ctx: 2048, n_threads: 1)
+    def initialize(model_path:, n_gpu_layers: 1, n_ctx: 2048, n_threads: 1, seed: -1)
       depends_on "llama_cpp"
       require "llama_cpp"
 
@@ -13,41 +13,59 @@ module Langchain::LLM
       @n_gpu_layers = n_gpu_layers
       @n_ctx = n_ctx
       @n_threads = n_threads
+      @seed = seed
     end
+
+    def embed(text:, n_threads: nil)
+      # contexts are kinda stateful when it comes to embeddings, so allocate one each time
+      context = build_context_params(embeddings: true)
+      embedding_input = context.tokenize(text: text, add_bos: true)
+      return unless embedding_input.size.positive?
+
+      n_threads ||= self.n_threads
+
+      context.eval(tokens: embedding_input, n_past: 0, n_threads: n_threads)
+      context.embeddings
+    end
+
+    def complete(prompt:, n_predict: 128, n_threads: nil)
+      n_threads ||= self.n_threads
+      # contexts do not appear to be stateful when it comes to completion, so re-use the same one
+      context = completion_context
+      ::LLaMACpp.generate(context, prompt, n_threads: n_threads, n_predict: n_predict)
+    end
+
+    private
 
     def n_threads
       # Use the maximum number of CPU threads available, if not configured
       @n_threads ||= `sysctl -n hw.ncpu`.strip.to_i
     end
 
-    def complete(prompt:, n_predict: 128, n_seed: -1, **params)
-      params = ::LLaMACpp::ContextParams.new
-      params.seed = n_seed
-      params.n_ctx = n_ctx
-      params.n_gpu_layers = n_gpu_layers
 
-      model = ::LLaMACpp::Model.new(model_path: model_path, params: params)
-      context = ::LLaMACpp::Context.new(model: model)
+    def build_context_params(embeddings: false)
+      context_params = ::LLaMACpp::ContextParams.new
 
-      ::LLaMACpp.generate(context, prompt, n_threads: n_threads, n_predict: n_predict)
+      context_params.seed = seed
+      context_params.n_ctx = n_ctx
+      context_params.n_gpu_layers = n_gpu_layers
+      context_params.embeddings = embeddings
+
+      context_params
     end
 
-    def embed(text:)
-      params = ::LLaMACpp::ContextParams.new
-      params.seed = 12
-      params.n_gpu_layers = n_gpu_layers
-      params.embedding = true
-
-      model = ::LLaMACpp::Model.new(model_path: model_path, params: params)
-      context = ::LLaMACpp::Context.new(model: model)
-
-      embedding_input = context.tokenize(text: text, add_bos: true)
-
-      return unless embedding_input.size.positive?
-
-      context.eval(tokens: embedding_input, n_past: 0, n_threads: n_threads)
-
-      context.embeddings
+    def build_model(embeddings: false)
+      return @model if defined?(@model)
+      @model = ::LLaMACpp::Model.new(model_path: model_path, params: build_context_params(embeddings: embeddings))
     end
+
+    def build_context
+      ::LLaMACpp::Context.new(model: build_model)
+    end
+
+    def completion_context
+      @completion_context ||= build_context
+    end
+
   end
 end
