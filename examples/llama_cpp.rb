e50ca13c025ed3a0a729b55bc1dd5d9001490927
Josh Nichols <josh.nichols@gusto.com>
2023-07-02 11:38:40 -0400

    Add a second instruction to example

--- examples/llama_cpp.rb
+++ examples/llama_cpp.rb
@@ -6,6 +6,19 @@ llm = Langchain::LLM::LlamaCpp.new(
   n_threads: Integer(ENV["LLAMACPP_N_THREADS"])
 )
 
-instruction = "Write a story about a pony who goes to the store to buy some apples."
-prompt = "#{instruction}\n\n### Response:"
-puts llm.complete prompt: prompt, n_predict: 1024
+instructions = [
+  "Tell me about the creator of Ruby",
+  "Write a story about a pony who goes to the store to buy some apples."
+]
+
+template = Langchain::Prompt::PromptTemplate.new(
+  template: "{instruction}\n\n### Response:",
+  input_variables: %w[instruction]
+)
+
+instructions.each do |instruction|
+  puts "USER: #{instruction}"
+  prompt = template.format(instruction: instruction)
+  response = llm.complete prompt: prompt, n_predict: 1024
+  puts "ASSISTANT: #{response}"
+end
