3c66f064e773f8340cb6c8217267f46720d217eb
Andrei Bondarev <andrei@sourcelabs.io>
2023-07-06 18:39:58 -0500

    Revert "Persist functions to conversation memory (#245)" (#246)
    This reverts commit 84fd3d06b994c2c6a70493f155b2e39272eabdef.

--- lib/langchain/conversation_memory.rb
+++ lib/langchain/conversation_memory.rb
@@ -2,23 +2,17 @@
 
 module Langchain
   class ConversationMemory
-    attr_reader :llm, :examples, :messages
-
-    attr_accessor :functions
+    attr_reader :examples, :messages
 
     # The least number of tokens we want to be under the limit by
     TOKEN_LEEWAY = 20
 
-    def initialize(llm:, messages: [], functions: [], **options)
+    def initialize(llm:, messages: [], **options)
       @llm = llm
       @context = nil
       @summary = nil
       @examples = []
       @messages = messages
-
-      @functions = functions
-      validate_functions_parameter!
-
       @strategy = options.delete(:strategy) || :truncate
       @options = options
     end
@@ -58,12 +52,6 @@ module Langchain
 
     private
 
-    def validate_functions_parameter!
-      if functions.any? && !llm.is_a?(Langchain::LLM::OpenAI)
-        raise ArgumentError, "Functions are currently only supported by Langchain::LLM::OpenAI"
-      end
-    end
-
     def truncate_messages(exception)
       raise exception if @messages.size == 1
 
@@ -71,7 +59,7 @@ module Langchain
 
       @messages = @messages.drop_while do |message|
         proceed = token_overflow > -TOKEN_LEEWAY
-        token_overflow -= token_length(message.to_json, model_name, llm: llm)
+        token_overflow -= token_length(message.to_json, model_name, llm: @llm)
 
         proceed
       end
@@ -81,7 +69,7 @@ module Langchain
       history = [@summary, @messages.to_json].compact.join("\n")
       partitions = [history[0, history.size / 2], history[history.size / 2, history.size]]
 
-      @summary = partitions.map { |messages| llm.summarize(text: messages.to_json) }.join("\n")
+      @summary = partitions.map { |messages| @llm.summarize(text: messages.to_json) }.join("\n")
 
       @messages = [@messages.last]
     end
@@ -90,11 +78,11 @@ module Langchain
     end
 
     def model_name
-      llm.class::DEFAULTS[:chat_completion_model_name]
+      @llm.class::DEFAULTS[:chat_completion_model_name]
     end
 
     def token_length(content, model_name, options)
-      llm.class::LENGTH_VALIDATOR.token_length(content, model_name, options)
+      @llm.class::LENGTH_VALIDATOR.token_length(content, model_name, options)
     end
   end
 end
