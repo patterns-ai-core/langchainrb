2cb698a74b9a0e6c5d4ae005b7465f82fd18da0d
Andrei Bondarev <andrei@sourcelabs.io>
2023-07-06 12:37:29 -0500

    Adding llama_cpp.rb support (#236)
    * Adding llama_cpp.rb support
    
    * Start an example to use
    
    * Expose more llama.cpp options
    
    - n_ctx, for the context window size
    - n_gpu_layers, for layers to send to gpu
    - n_threads, for number of threads to use
    - seed, value to use for seeding random data
    
    Also default n_gpu_layers to 1, instead of number of GPU cores
    
    * Add a second instruction to example
    
    * include seeds. try to control lifecycle of various llama.cpp to avoid having to allocate unless necessary
    
    * tiny refactor
    
    * Loose llama_cpp version due to rapidly evolving underlying lib
    
    * fix linter
    
    * LlamaCpp tests
    
    ---------
    
    Co-authored-by: Josh Nichols <josh.nichols@gusto.com>

---
+++ spec/langchain/llm/llama_cpp_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require "llama_cpp"
+
+RSpec.describe Langchain::LLM::LlamaCpp do
+  subject { described_class.new(model_path: "./", seed: 1) }
+
+  let(:llama_context) { double("LLaMACpp::Context") }
+  let(:llama_model) { double("LLaMACpp::Model") }
+  let(:llama_context_params) { double("LLaMACpp::ContextParams") }
+
+  before do
+    allow(LLaMACpp::Model).to receive(:new).and_return(llama_model)
+    allow(LLaMACpp::Context).to receive(:new).and_return(llama_context)
+  end
+
+  describe "#embed" do
+    let(:embedding) { [0.1, 0.2, 0.3] }
+
+    before do
+      allow(llama_context).to receive(:tokenize).and_return([1, 9029])
+      allow(llama_context).to receive(:eval)
+      allow(llama_context).to receive(:embeddings).and_return(embedding)
+    end
+
+    it "generates an embedding" do
+      expect(subject.embed(text: "Hello World")).to eq(embedding)
+    end
+  end
+
+  describe "#complete" do
+    let(:completion) { "I'm doing well. How about you?" }
+
+    before do
+      allow(LLaMACpp).to receive(:generate).and_return(completion)
+    end
+
+    it "generates a completion" do
+      expect(subject.complete(prompt: "Hello! How are you?")).to eq(completion)
+    end
+  end
+end
