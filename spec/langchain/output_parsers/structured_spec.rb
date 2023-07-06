0e347830dac0101624a097cfc8557ed2aebb9aab
Josh Weir <joshua.weir@gmail.com>
2023-07-06 22:15:39 +1000

    feat: add Langchain::OutputParsers::OutputFixingParser to wrap a Langchain::OutputParser and handle invalid response (#220)
    * feat: add Langchain::OutputParsers::StructuredOutputParser to tame LLM output to JSON specification
    
    * similar approach to langchainjs StructuredOutputParser (https://github.com/hwchase17/langchainjs/blob/3f75f819faea30c6b578bb7901f7868d6f2cfaa2/langchain/src/output_parsers/structured.ts) uses json-schema gem (JSON::Schema) in place of zod schema
    * add Langchain::OutputParsers::StructuredOutputParser which receives JSON::Schema on initialization
    	* provides #get_format_i
    nstructions to instruct the LLM to conform it's output to the JSON::Schema
    	* provides #parse to parse the LLM output and validate against the JSON::Schema
    
    * feat: add example usage
    
    * add comments:
    
    * fix: typo in example comment
    
    * fix: change usage of @schema to schema as an attr_reader is defined
    
    * fix: change validate_schema to validate_schema! to better communicate methods potentially error-raising behavior
    
    * WIP
    
    * feat: add Langchain::OutputParsers::OutputFixingParser to wrap a Langchain::OutputParser and handle invalid response
    
    * similar approach to langchainjs OutputFixingParser (https://js.langchain.com/docs/modules/prompts/output_parsers/#output-fixing-parser)
    * add tests
    * add example usage
    
    * fix: remove unrequired comment
    
    * fix: refactor to read naive prompt template from yaml file
    
    * feat: add Output Parsers section to the README
    
    ---------
    
    Co-authored-by: Andrei Bondarev <andrei@sourcelabs.io>

--- spec/langchain/output_parsers/structured_spec.rb
+++ spec/langchain/output_parsers/structured_spec.rb
@@ -1,66 +1,8 @@
 # frozen_string_literal: true
 
+require_relative "spec_helper"
+
 RSpec.describe Langchain::OutputParsers::StructuredOutputParser do
-  let!(:schema_example) do
-    {
-      type: "object",
-      properties: {
-        name: {
-          type: "string",
-          description: "Persons name"
-        },
-        age: {
-          type: "number",
-          description: "Persons age"
-        },
-        interests: {
-          type: "array",
-          items: {
-            type: "object",
-            properties: {
-              interest: {
-                type: "string",
-                description: "A topic of interest"
-              },
-              levelOfInterest: {
-                type: "number",
-                description: "A value between 0 and 100 of how interested the person is in this interest"
-              }
-            },
-            required: ["interest", "levelOfInterest"],
-            additionalProperties: false
-          },
-          minItems: 1,
-          maxItems: 3,
-          description: "A list of the person's interests"
-        }
-      },
-      required: ["name", "age", "interests"],
-      additionalProperties: false
-    }
-  end
-
-  let!(:json_response) do
-    {
-      "name" => "Hayes Weir",
-      "age" => 2,
-      "interests" => [
-        {
-          "interest" => "Dinosaurs",
-          "levelOfInterest" => 90
-        },
-        {
-          "interest" => "Sugar",
-          "levelOfInterest" => 95
-        }
-      ]
-    }
-  end
-
-  let!(:json_text_response) do
-    json_response.to_json
-  end
-
   let!(:json_with_backticks_text_response) do
     <<~RESPONSE
       I'm responding with a narrative even though you asked for only json response:
@@ -152,16 +94,11 @@ RSpec.describe Langchain::OutputParsers::StructuredOutputParser do
     it "fails to parse response text if the json does not conform to the schema" do
       parser = described_class.from_json_schema(schema_example)
       expect {
-        parser.parse(
-          <<~RESPONSE
-            {
-              "name": "Elon",
-              "age": 51,
-              "interests": []
-            }
-          RESPONSE
-        )
-      }.to raise_error(Langchain::OutputParsers::OutputParserException, /'#\/interests' did not contain a minimum number of items/)
+        parser.parse(invalid_schema_json_text_response)
+      }.to raise_error(
+        Langchain::OutputParsers::OutputParserException,
+        /'#\/interests' did not contain a minimum number of items/
+      )
     end
   end
 
