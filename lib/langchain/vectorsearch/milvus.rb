af3369b411d004d934e7429c815f6bea43a49153
Andrei Bondarev <andrei@sourcelabs.io>
2023-08-05 21:32:59 -0400

    Milvus fixes (#272)
    * Milvus fixes
    
    * fix spec

--- lib/langchain/vectorsearch/milvus.rb
+++ lib/langchain/vectorsearch/milvus.rb
@@ -32,7 +32,7 @@ module Langchain::Vectorsearch
             field: Array(texts)
           }, {
             field_name: "vectors",
-            type: ::Milvus::DATA_TYPES["binary_vector"],
+            type: ::Milvus::DATA_TYPES["float_vector"],
             field: Array(texts).map { |text| llm.embed(text: text) }
           }
         ]
@@ -47,7 +47,7 @@ module Langchain::Vectorsearch
       client.collections.create(
         auto_id: true,
         collection_name: index_name,
-        description: "Default schema created by Vectorsearch",
+        description: "Default schema created by langchain.rb",
         fields: [
           {
             name: "id",
@@ -66,7 +66,7 @@ module Langchain::Vectorsearch
             ]
           }, {
             name: "vectors",
-            data_type: ::Milvus::DATA_TYPES["binary_vector"],
+            data_type: ::Milvus::DATA_TYPES["float_vector"],
             is_primary_key: false,
             type_params: [
               {
@@ -75,6 +75,20 @@ module Langchain::Vectorsearch
               }
             ]
           }
+        ]
+      )
+    end
+
+    # Create the default index
+    # @return [Boolean] The response from the server
+    def create_default_index
+      client.indices.create(
+        collection_name: "Documents",
+        field_name: "vectors",
+        extra_params: [
+          {key: "metric_type", value: "L2"},
+          {key: "index_type", value: "IVF_FLAT"},
+          {key: "params", value: "{\"nlist\":1024}"}
         ]
       )
     end
@@ -91,6 +105,12 @@ module Langchain::Vectorsearch
       client.collections.delete(collection_name: index_name)
     end
 
+    # Load default schema into memory
+    # @return [Boolean] The response from the server
+    def load_default_schema
+      client.collections.load(collection_name: index_name)
+    end
+
     def similarity_search(query:, k: 4)
       embedding = llm.embed(text: query)
 
@@ -101,15 +121,36 @@ module Langchain::Vectorsearch
     end
 
     def similarity_search_by_vector(embedding:, k: 4)
+      load_default_schema
+
       client.search(
         collection_name: index_name,
+        output_fields: ["id", "content", "vectors"],
         top_k: k.to_s,
         vectors: [embedding],
         dsl_type: 1,
         params: "{\"nprobe\": 10}",
-        anns_field: "content",
-        metric_type: "L2"
+        anns_field: "vectors",
+        metric_type: "L2",
+        vector_type: ::Milvus::DATA_TYPES["float_vector"]
       )
+    end
+
+    # Ask a question and return the answer
+    # @param question [String] The question to ask
+    # @yield [String] Stream responses back one String at a time
+    # @return [String] The answer to the question
+    def ask(question:, &block)
+      search_results = similarity_search(query: question)
+
+      content_field = search_results.dig("results", "fields_data").select { |field| field.dig("field_name") == "content" }
+      content_data = content_field.first.dig("Field", "Scalars", "Data", "StringData", "data")
+
+      context = content_data.join("\n---\n")
+
+      prompt = generate_prompt(question: question, context: context)
+
+      llm.chat(prompt: prompt, &block)
     end
   end
 end
