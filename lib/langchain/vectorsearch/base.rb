d06751d9fb1a1e83abee6577ac08d59192e05568
Andrew Morrison <morro141@gmail.com>
2023-08-23 16:49:18 -0400

    add namespace to add_data method

--- lib/langchain/vectorsearch/base.rb
+++ lib/langchain/vectorsearch/base.rb
@@ -160,7 +160,7 @@ module Langchain::Vectorsearch
       prompt_template.format(question: question)
     end
 
-    def add_data(paths:)
+    def add_data(paths:, namespace: "")
       raise ArgumentError, "Paths must be provided" if Array(paths).empty?
 
       texts = Array(paths)
@@ -172,7 +172,7 @@ module Langchain::Vectorsearch
 
       texts.flatten!
 
-      add_texts(texts: texts)
+      add_texts(texts: texts, namespace: namespace)
     end
 
     def self.logger_options
