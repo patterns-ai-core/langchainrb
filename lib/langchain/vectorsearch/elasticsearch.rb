d6f4d7d5e35d358b61ffd733b450480a2bf46144
Siva Gollapalli <sivagollapalli88@gmail.com>
2023-10-29 22:00:20 +0530

    [#358] [WIP] Added ask functionality

--- lib/langchain/vectorsearch/elasticsearch.rb
+++ lib/langchain/vectorsearch/elasticsearch.rb
@@ -116,9 +116,23 @@ module Langchain::Vectorsearch
       }
     end
 
-    # TODO: Implement this
-    # def ask()
-    # end
+    # Ask a question and return the answer
+    # @param question [String] The question to ask
+    # @param k [Integer] The number of results to have in context
+    # @yield [String] Stream responses back one String at a time
+    # @return [String] The answer to the question
+    def ask(question:, k: 4, &block)
+      search_results = similarity_search(query: question, k: k)
+
+      context = search_results.map do |result|
+        result.content.to_s
+      end
+      context = context.join("\n---\n")
+
+      prompt = generate_rag_prompt(question: question, context: context)
+
+      llm.chat(prompt: prompt, &block)
+    end
 
     def similarity_search(text: "", k: 10, query: {})
       if text.empty? && query.empty?
