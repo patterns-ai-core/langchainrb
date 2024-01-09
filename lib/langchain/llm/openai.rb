43cbd57155ac5fbd709315bb78772ce0d5dd09c3
Matt Lindsey <matthew.r.lindsey@gmail.com>
2024-01-09 18:08:56 -0500

    Handle openai stop param

--- lib/langchain/llm/openai.rb
+++ lib/langchain/llm/openai.rb
@@ -78,6 +78,10 @@ module Langchain::LLM
     # @return [Langchain::LLM::OpenAIResponse] Response object
     def complete(prompt:, **params)
       # Should we still accept the `messages: []` parameter here?
+      if params[:stop_sequences]
+        params[:stop] = params.delete(:stop_sequences)
+      end
+
       messages = [{role: "user", content: prompt}]
       chat(messages: messages, **params)
     end
