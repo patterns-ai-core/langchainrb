75e60504c55a6f9e11ac64b9d107d8faa86d0f94
John Knapp <1114452+johnknapp@users.noreply.github.com>
2024-01-26 12:27:37 -0800

    Update openai_validator.rb
    trailing white space fix

--- lib/langchain/utils/token_length/openai_validator.rb
+++ lib/langchain/utils/token_length/openai_validator.rb
@@ -41,7 +41,7 @@ module Langchain
           "gpt-4-32k" => 32768,
           "gpt-4-32k-0314" => 32768,
           "gpt-4-32k-0613" => 32768,
-          "gpt-4-1106-preview" => 128000,          
+          "gpt-4-1106-preview" => 128000,
           "gpt-4-turbo-preview" => 128000,
           "gpt-4-0125-preview" => 128000,
           "gpt-4-vision-preview" => 128000,
