06be7c4ba21eae1e66c46ac70b8df6081288742f
Felix Letkemann (IoT Venture) <f.letkemann@iot-venture.com>
2024-01-29 15:39:18 +0100

    adding max token length of new gpt4 turbo model

--- lib/langchain/utils/token_length/openai_validator.rb
+++ lib/langchain/utils/token_length/openai_validator.rb
@@ -42,6 +42,8 @@ module Langchain
           "gpt-4-32k-0314" => 32768,
           "gpt-4-32k-0613" => 32768,
           "gpt-4-1106-preview" => 128000,
+          "gpt-4-0125-preview" => 128000,
+          "gpt-4-turbo-preview" => 128000,
           "gpt-4-vision-preview" => 128000,
           "text-curie-001" => 2049,
           "text-babbage-001" => 2049,
