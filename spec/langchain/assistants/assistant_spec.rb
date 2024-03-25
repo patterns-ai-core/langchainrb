cc2629ce802443b65dfdcc157aa81c598ecca558
Matt Lindsey <matthew.r.lindsey@gmail.com>
2024-03-25 12:13:53 -0400

    Add tests for Assistant run (#540)
    * Add more assistant tests
    
    * Clean up the spec
    
    ---------
    
    Co-authored-by: Andrei Bondarev <andrei@sourcelabs.io>
    Co-authored-by: Andrei Bondarev <andrei.bondarev13@gmail.com>

--- spec/langchain/assistants/assistant_spec.rb
+++ spec/langchain/assistants/assistant_spec.rb
@@ -50,4 +50,80 @@ RSpec.describe Langchain::Assistant do
       expect(thread.messages.last.content).to eq("bar")
     end
   end
+
+  describe "#run" do
+    let(:raw_openai_response) do
+      {
+        "id" => "chatcmpl-96QTYLFcp0haHHRhnqvTYL288357W",
+        "object" => "chat.completion",
+        "created" => 1711318768,
+        "model" => "gpt-3.5-turbo-0125",
+        "choices" => [
+          {
+            "index" => 0,
+            "message" => {
+              "role" => "assistant",
+              "content" => nil,
+              "tool_calls" => [
+                {
+                  "id" => "call_9TewGANaaIjzY31UCpAAGLeV",
+                  "type" => "function",
+                  "function" => {"name" => "calculator-execute", "arguments" => "{\"input\":\"2+2\"}"}
+                }
+              ]
+            },
+            "logprobs" => nil,
+            "finish_reason" => "tool_calls"
+          }
+        ],
+        "usage" => {"prompt_tokens" => 91, "completion_tokens" => 18, "total_tokens" => 109},
+        "system_fingerprint" => "fp_3bc1b5746b"
+      }
+    end
+
+    context "when auto_tool_execution is false" do
+      it "runs the assistant" do
+        allow(subject.llm).to receive(:chat).and_return(Langchain::LLM::OpenAIResponse.new(raw_openai_response))
+
+        subject.add_message(role: "user", content: "Please calculate 2+2")
+
+        subject.run(auto_tool_execution: false)
+
+        expect(subject.thread.messages.last.role).to eq("assistant")
+        expect(subject.thread.messages.last.tool_calls).to eq([raw_openai_response["choices"][0]["message"]["tool_calls"]][0])
+      end
+    end
+
+    context "when auto_tool_execution is true" do
+      let(:raw_openai_response2) do
+        {
+          "id" => "chatcmpl-96P6eEMDDaiwzRIHJZAliYHQ8ov3q",
+          "object" => "chat.completion",
+          "created" => 1711313504,
+          "model" => "gpt-3.5-turbo-0125",
+          "choices" => [{"index" => 0, "message" => {"role" => "assistant", "content" => "The result of 2 + 2 is 4."}, "logprobs" => nil, "finish_reason" => "stop"}],
+          "usage" => {"prompt_tokens" => 121, "completion_tokens" => 13, "total_tokens" => 134},
+          "system_fingerprint" => "fp_3bc1b5746c"
+        }
+      end
+
+      it "runs the assistant and automatically executes tool calls" do
+        allow(subject.llm).to receive(:chat).and_return(Langchain::LLM::OpenAIResponse.new(raw_openai_response2))
+        allow(subject.tools[0]).to receive(:execute).with(
+          input: "2+2"
+        ).and_return("4.0")
+
+        subject.add_message(role: "user", content: "Please calculate 2+2")
+        subject.add_message(role: "assistant", tool_calls: raw_openai_response["choices"][0]["message"]["tool_calls"])
+
+        subject.run(auto_tool_execution: true)
+
+        expect(subject.thread.messages[-2].role).to eq("tool")
+        expect(subject.thread.messages[-2].content).to eq("4.0")
+
+        expect(subject.thread.messages[-1].role).to eq("assistant")
+        expect(subject.thread.messages[-1].content).to eq("The result of 2 + 2 is 4.")
+      end
+    end
+  end
 end
