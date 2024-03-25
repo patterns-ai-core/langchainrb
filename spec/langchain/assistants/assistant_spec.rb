bb34d4ac8c18bbc56099ef59501c1411a8fb4e5d
Andrei Bondarev <andrei.bondarev13@gmail.com>
2024-03-25 12:02:31 -0400

    Clean up the spec

--- spec/langchain/assistants/assistant_spec.rb
+++ spec/langchain/assistants/assistant_spec.rb
@@ -52,74 +52,69 @@ RSpec.describe Langchain::Assistant do
   end
 
   describe "#run" do
-    let(:openai_response) do
+    let(:raw_openai_response) do
       {
-        raw_response: {
-          "id" => "chatcmpl-96QTYLFcp0haHHRhnqvTYL288357W",
-          "object" => "chat.completion",
-          "created" => 1711318768,
-          "model" => "gpt-3.5-turbo-0125",
-          "choices" =>
-       [{"index" => 0,
-         "message" =>
-              {"role" => "assistant",
-               "content" => nil,
-               "tool_calls" =>
-            [{"id" => "call_9TewGANaaIjzY31UCpAAGLeV",
-              "type" => "function",
-              "function" => {"name" => "calculator-execute", "arguments" => "{\"input\":\"2+2\"}"}}]},
-         "logprobs" => nil,
-         "finish_reason" => "tool_calls"}],
-          "usage" => {"prompt_tokens" => 91, "completion_tokens" => 18, "total_tokens" => 109},
-          "system_fingerprint" => "fp_3bc1b5746b"
-        },
-        model: "gpt-3.5-turbo-0125"
+        "id" => "chatcmpl-96QTYLFcp0haHHRhnqvTYL288357W",
+        "object" => "chat.completion",
+        "created" => 1711318768,
+        "model" => "gpt-3.5-turbo-0125",
+        "choices" => [
+          {
+            "index" => 0,
+            "message" => {
+              "role" => "assistant",
+              "content" => nil,
+              "tool_calls" => [
+                {
+                  "id" => "call_9TewGANaaIjzY31UCpAAGLeV",
+                  "type" => "function",
+                  "function" => {"name" => "calculator-execute", "arguments" => "{\"input\":\"2+2\"}"}
+                }
+              ]
+            },
+            "logprobs" => nil,
+            "finish_reason" => "tool_calls"
+          }
+        ],
+        "usage" => {"prompt_tokens" => 91, "completion_tokens" => 18, "total_tokens" => 109},
+        "system_fingerprint" => "fp_3bc1b5746b"
       }
     end
 
     context "when auto_tool_execution is false" do
       it "runs the assistant" do
-        allow(subject.llm).to receive(:chat).and_return(openai_response)
-        allow(openai_response).to receive(:tool_calls).and_return(openai_response[:raw_response]["choices"][0]["message"]["tool_calls"])
-        allow(openai_response).to receive(:chat_completion).and_return(openai_response[:raw_response]["choices"][0]["message"]["content"])
-        allow(openai_response).to receive(:role).and_return(openai_response[:raw_response]["choices"][0]["message"]["role"])
+        allow(subject.llm).to receive(:chat).and_return(Langchain::LLM::OpenAIResponse.new(raw_openai_response))
 
         subject.add_message(role: "user", content: "Please calculate 2+2")
 
         subject.run(auto_tool_execution: false)
 
-        expect(subject.thread.messages[-1].role).to eq("assistant")
-        expect(subject.thread.messages[-1].tool_calls).to eq([openai_response[:raw_response]["choices"][0]["message"]["tool_calls"]][0])
+        expect(subject.thread.messages.last.role).to eq("assistant")
+        expect(subject.thread.messages.last.tool_calls).to eq([raw_openai_response["choices"][0]["message"]["tool_calls"]][0])
       end
     end
 
     context "when auto_tool_execution is true" do
-      let(:openai_response2) do
+      let(:raw_openai_response2) do
         {
-          raw_response: {
-            "id" => "chatcmpl-96P6eEMDDaiwzRIHJZAliYHQ8ov3q",
-            "object" => "chat.completion",
-            "created" => 1711313504,
-            "model" => "gpt-3.5-turbo-0125",
-            "choices" => [{"index" => 0, "message" => {"role" => "assistant", "content" => "The result of 2 + 2 is 4."}, "logprobs" => nil, "finish_reason" => "stop"}],
-            "usage" => {"prompt_tokens" => 121, "completion_tokens" => 13, "total_tokens" => 134},
-            "system_fingerprint" => "fp_3bc1b5746c"
-          },
-          model: "gpt-3.5-turbo-0125"
+          "id" => "chatcmpl-96P6eEMDDaiwzRIHJZAliYHQ8ov3q",
+          "object" => "chat.completion",
+          "created" => 1711313504,
+          "model" => "gpt-3.5-turbo-0125",
+          "choices" => [{"index" => 0, "message" => {"role" => "assistant", "content" => "The result of 2 + 2 is 4."}, "logprobs" => nil, "finish_reason" => "stop"}],
+          "usage" => {"prompt_tokens" => 121, "completion_tokens" => 13, "total_tokens" => 134},
+          "system_fingerprint" => "fp_3bc1b5746c"
         }
       end
 
       it "runs the assistant and automatically executes tool calls" do
-        allow(subject.llm).to receive(:chat).and_return(openai_response2)
-        allow(openai_response2).to receive(:tool_calls).and_return(nil)
-        allow(openai_response2).to receive(:chat_completion).and_return(openai_response2[:raw_response]["choices"][0]["message"]["content"])
-        allow(openai_response2).to receive(:role).and_return(openai_response2[:raw_response]["choices"][0]["message"]["role"])
+        allow(subject.llm).to receive(:chat).and_return(Langchain::LLM::OpenAIResponse.new(raw_openai_response2))
         allow(subject.tools[0]).to receive(:execute).with(
           input: "2+2"
         ).and_return("4.0")
 
         subject.add_message(role: "user", content: "Please calculate 2+2")
-        subject.add_message(role: "assistant", tool_calls: openai_response[:raw_response]["choices"][0]["message"]["tool_calls"])
+        subject.add_message(role: "assistant", tool_calls: raw_openai_response["choices"][0]["message"]["tool_calls"])
 
         subject.run(auto_tool_execution: true)
 
