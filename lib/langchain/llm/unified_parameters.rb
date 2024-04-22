49062b9160a32ea0cd9656269c2437fe87c3b686
Valentino Stoll <vstoll@doximity.com>
2024-04-22 09:39:42 -0400

    Adds Langchain::LLM::UnifiedParameters to prepare for unifying LLM APIs

---
+++ lib/langchain/llm/unified_parameters.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Langchain::LLM
+  class UnifiedParameters
+    attr_reader :schema, :aliases, :parameters
+
+    def initialize(schema:, aliases: {}, parameters: {})
+      @schema = schema || {}
+      @aliases = aliases || {}
+      @parameters = to_params(parameters.to_h) if !parameters.to_h.empty?
+    end
+
+    def to_params(params = {})
+      @parameters ||= params.slice(*schema.keys)
+      @aliases.each do |new_key, existing_key|
+        # favor existing keys in case of conflicts
+        @parameters[existing_key] ||= params[new_key]
+      end
+      @parameters
+    end
+
+    def to_h
+      @parameters.to_h
+    end
+  end
+end
---
+++ spec/langchain/llm/unified_parameters_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+RSpec.describe Langchain::LLM::UnifiedParameters do
+  # For now, the unifier only maps keys, but in the future it may be beneficial
+  # to introduce an ActiveModel-style validator to restrict inputs to conform to
+  # types required of the LLMs APIs
+  let(:schema) do
+    {beep: Integer, boop: Integer}
+  end
+  let(:aliases) do
+    {booop: :boop}
+  end
+  let(:subject) { described_class.new(schema: schema, aliases: aliases) }
+
+  describe "#to_params" do
+    let(:params) do
+      {beep: 1, boop: 2, bop: 3}
+    end
+
+    it "filters out params not in the schema" do
+      expect(subject.to_params(params)).to match(beep: 1, boop: 2)
+    end
+
+    context "with aliases" do
+      let(:params) do
+        {beep: 1, booop: 4, bop: 3}
+      end
+      it "translates alias schema fields" do
+        expect(subject.to_params(params)).to match(beep: 1, boop: 4)
+      end
+    end
+
+    context "with alias conflicts" do
+      let(:params) do
+        {beep: 1, boop: 2, bop: 3, booop: 4}
+      end
+      it "favors explicit params over aliases" do
+        expect(subject.to_params(params)).to match(beep: 1, boop: 2)
+      end
+    end
+  end
+
+  describe "#to_h" do
+    let(:subject) do
+      described_class.new(
+        schema: schema,
+        aliases: aliases,
+        parameters: {beep: 1, boop: 2, bop: 3}
+      )
+    end
+    it "aliases to_params" do
+      expect(subject.to_h).to match(beep: 1, boop: 2)
+    end
+  end
+end
