ee9307c38db66f89e2c092bb5c65b9c5a254ca63
Matt Lindsey <matthew.r.lindsey@gmail.com>
2024-05-04 14:47:58 -0400

    Add print to assistant example

--- examples/assistant_chat.rb
+++ examples/assistant_chat.rb
@@ -6,32 +6,32 @@ require "reline"
 # gem install reline
 # or add `gem "reline"` to your Gemfile
 
-openai = Langchain::LLM::OpenAI.new(api_key: ENV["OPENAI_API_KEY"])
-thread = Langchain::Thread.new
-assistant = Langchain::Assistant.new(
-  llm: openai,
-  thread: thread,
-  instructions: "You are a Meteorologist Assistant that is able to pull the weather for any location",
+@assistant = Langchain::Assistant.new(
+  llm: Langchain::LLM::OpenAI.new(api_key: ENV["OPENAI_API_KEY"]),
+  thread: Langchain::Thread.new,
+  instructions: "You are a Meteorologist Assistant that is able to pull the weather for any city in imperial units.",
   tools: [
     Langchain::Tool::Weather.new(api_key: ENV["OPEN_WEATHER_API_KEY"])
   ]
 )
 
-DONE = %w[done end eof exit].freeze
+DONE = %w[done end eof print exit].freeze
 
 puts "Welcome to your Meteorological assistant!"
 
 def prompt_for_message
-  puts "(multiline input; type 'end' on its own line when done. or exit to exit)"
+  puts "(multiline input; type 'end' on its own line when done. or 'print' to print thread, or 'exit' to exit)"
 
-  user_message = Reline.readmultiline("Question: ", true) do |multiline_input|
+  user_message = Reline.readmultiline("User message: ", true) do |multiline_input|
     last = multiline_input.split.last
     DONE.include?(last)
   end
 
   return :noop unless user_message
+  return :print if user_message == "print"
 
   lines = user_message.split("\n")
+
   if lines.size > 1 && DONE.include?(lines.last)
     # remove the "done" from the message
     user_message = lines[0..-2].join("\n")
@@ -42,6 +42,22 @@ def prompt_for_message
   user_message
 end
 
+def print_thread
+  @assistant.thread.messages.each do |message|
+    puts "----"
+    puts message.role + ": " + message.content
+    case message.role
+    when "assistant"
+      message.tool_calls.each do |tool_call|
+        puts " " + tool_call["function"]["name"] + ": " << tool_call["function"]["arguments"]
+      end
+    when "tool"
+      puts " " + message.tool_call_id
+    end
+  end
+  puts "---"
+end
+
 begin
   loop do
     user_message = prompt_for_message
@@ -49,12 +65,15 @@ begin
     case user_message
     when :noop
       next
+    when :print
+      print_thread
+      next
     when :exit
       break
     end
 
-    assistant.add_message_and_run content: user_message, auto_tool_execution: true
-    puts assistant.thread.messages.last.content
+    @assistant.add_message_and_run content: user_message, auto_tool_execution: true
+    puts @assistant.thread.messages.last.content
   end
 rescue Interrupt
   exit 0
