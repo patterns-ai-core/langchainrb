3985f993ee5615cfcb263fb871b950ba17223b0e
Andrei Bondarev <andrei.bondarev13@gmail.com>
2024-05-14 11:04:17 -0400

    Fix specs

--- spec/langchain/llm/google_vertex_ai_spec.rb
+++ spec/langchain/llm/google_vertex_ai_spec.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
 require "googleauth"
+require "httparty"
 
 RSpec.describe Langchain::LLM::GoogleVertexAI do
   let(:subject) { described_class.new(project_id: "123", region: "us-central1") }
 
   describe "#embed" do
     let(:embedding) { [-0.00879860669374466, 0.007578692398965359, 0.021136576309800148] }
-    let(:raw_embedding_response) { JSON.parse(File.read("spec/fixtures/llm/google_vertex_ai/embed.json"), symbolize_names: true) }
+    let(:raw_embedding_response) { JSON.parse(File.read("spec/fixtures/llm/google_vertex_ai/embed.json")) }
 
     before do
       allow(Google::Auth).to receive(:get_application_default).and_return(
-        double("Google::Auth::UserRefreshCredentials")
+        double("Google::Auth::UserRefreshCredentials", fetch_access_token!: {access_token: 123})
       )
 
       allow(HTTParty).to receive(:post).and_return(raw_embedding_response)
@@ -23,7 +24,6 @@ RSpec.describe Langchain::LLM::GoogleVertexAI do
       expect(response).to be_a(Langchain::LLM::GoogleGeminiResponse)
       expect(response.model).to eq("textembedding-gecko")
       expect(response.embedding).to eq(embedding)
-      expect(response.total_tokens).to eq(3)
     end
   end
 end
