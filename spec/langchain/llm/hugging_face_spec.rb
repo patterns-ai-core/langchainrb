ef2f0d6974b87ea84df8e6e81490bf0c678e99f4
Brice Sanchez <bricesanchez@users.noreply.github.com>
2024-05-15 09:57:52 -0400

    fix: re-add default_dimensions

--- spec/langchain/llm/hugging_face_spec.rb
+++ spec/langchain/llm/hugging_face_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Langchain::LLM::HuggingFace do
   let(:subject) { described_class.new(api_key: "123") }
 
   describe "#embed" do
-
     context "when the embeddings_model_name is not passed as an argument" do
       let(:embedding) {
         [-0.03447720780968666, 0.031023189425468445, 0.006734968163073063]
@@ -41,6 +40,25 @@ RSpec.describe Langchain::LLM::HuggingFace do
 
       it "returns an embedding" do
         expect(subject.embed(text: "Hello World").embedding).to eq(embedding)
+      end
+    end
+  end
+
+  describe "#default_dimensions" do
+    it "returns the default dimensions" do
+      expect(subject.default_dimensions).to eq(384)
+    end
+
+    context "when the dimensions is passed as an argument" do
+      let(:subject) do
+        described_class.new(api_key: "123", default_options: {
+          embeddings_model_name: "mixedbread-ai/mxbai-embed-large-v1",
+          dimensions: 1_024
+        })
+      end
+
+      it "sets the default_dimensions" do
+        expect(subject.default_dimensions).to eq 1_024
       end
     end
   end
