ac638cfb3d305a301de48cfc752ada9323a11485
Brice Sanchez <bricesanchez@users.noreply.github.com>
2024-05-15 08:28:29 -0400

    feat(hugging_face): Add support for multiple models and dimensions
    see: https://github.com/alchaplinsky/hugging-face/commit/bee85a443bd085acbc61ad8926afa574aadfc7a4

--- spec/langchain/llm/hugging_face_spec.rb
+++ spec/langchain/llm/hugging_face_spec.rb
@@ -15,11 +15,46 @@ RSpec.describe Langchain::LLM::HuggingFace do
     it "returns an embedding" do
       expect(subject.embed(text: "Hello World").embedding).to eq([-1.5693359, -0.9458008, 1.9355469])
     end
+
+    context "when the embeddings_model_name is passed as an argument" do
+      let(:subject) {
+        described_class.new(
+          api_key: "123",
+          default_options: {
+            embeddings_model_name: "mixedbread-ai/mxbai-embed-large-v1",
+            dimensions: 1_024
+          }
+        )
+      }
+
+      it "returns an embedding" do
+        expect(subject.embed(text: "Hello World").embedding).to eq([-1.5693359, -0.9458008, 1.9355469])
+      end
+    end
   end
 
   describe "#default_dimensions" do
     it "returns the default dimensions" do
       expect(subject.default_dimensions).to eq(384)
+    end
+  end
+
+  describe "#default_dimensions" do
+    it "returns the default dimensions" do
+      expect(subject.default_dimensions).to eq(384)
+    end
+
+    context "when the dimensions is passed as an argument" do
+      let(:subject) do
+        described_class.new(api_key: "123", default_options: {
+          embeddings_model_name: "mixedbread-ai/mxbai-embed-large-v1",
+          dimensions: 1_024
+        })
+      end
+
+      it "sets the default_dimensions" do
+        expect(subject.default_dimensions).to eq 1_024
+      end
     end
   end
 end
