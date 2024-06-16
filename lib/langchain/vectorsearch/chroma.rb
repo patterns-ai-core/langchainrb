7ae1b48569b1ff4a42114d3cc87f01e9efdfd32a
Andrei Bondarev <andrei.bondarev13@gmail.com>
2024-06-16 16:54:02 -0400

    Fix Chroma#remove_texts method

--- lib/langchain/vectorsearch/chroma.rb
+++ lib/langchain/vectorsearch/chroma.rb
@@ -64,7 +64,7 @@ module Langchain::Vectorsearch
     # @param ids [Array<String>] The list of ids to remove
     # @return [Hash] The response from the server
     def remove_texts(ids:)
-      collection.delete(ids)
+      collection.delete(ids: ids)
     end
 
     # Create the collection with the default schema
