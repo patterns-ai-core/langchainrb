7ddf5abd7130d8c1070f7524308640e1e340202d
Andrei Bondarev <andrei.bondarev13@gmail.com>
2024-07-24 22:44:30 -0400

    Add spec for #symbolize_keys()

--- spec/langchain/utils/hash_transformer_spec.rb
+++ spec/langchain/utils/hash_transformer_spec.rb
@@ -64,4 +64,59 @@ RSpec.describe Langchain::Utils::HashTransformer do
       expect(result).to eq({levelOne: {levelTwo: {levelThree: {nestedKey: "value"}}}})
     end
   end
+
+  describe ".symbolize_keys" do
+    it "symbolizes string keys at the top level of the hash" do
+      hash = {"name" => "Alice", "age" => 30}
+      expected_hash = {name: "Alice", age: 30}
+      expect(described_class.symbolize_keys(hash)).to eq(expected_hash)
+    end
+
+    it "symbolizes string keys in nested hashes" do
+      hash = {
+        "user" => {
+          "name" => "Alice",
+          "details" => {
+            "age" => 30,
+            "city" => "Wonderland"
+          }
+        }
+      }
+      expected_hash = {
+        user: {
+          name: "Alice",
+          details: {
+            age: 30,
+            city: "Wonderland"
+          }
+        }
+      }
+      expect(described_class.symbolize_keys(hash)).to eq(expected_hash)
+    end
+
+    it "leaves keys that are already symbols unchanged" do
+      hash = {:name => "Alice", "age" => 30}
+      expected_hash = {name: "Alice", age: 30}
+      expect(described_class.symbolize_keys(hash)).to eq(expected_hash)
+    end
+
+    it "leaves keys that cannot be symbolized unchanged" do
+      hash = {Object.new => "value", "key" => "value"}
+      key = hash.keys.find { |k| k.is_a?(Object) }
+      expected_hash = {key => "value", :key => "value"}
+      expect(described_class.symbolize_keys(hash)).to eq(expected_hash)
+    end
+
+    it "returns an empty hash if the input hash is empty" do
+      hash = {}
+      expected_hash = {}
+      expect(described_class.symbolize_keys(hash)).to eq(expected_hash)
+    end
+
+    it "handles hashes with mixed key types" do
+      hash = {"string_key" => "value", :symbol_key => "value"}
+      expected_hash = {string_key: "value", symbol_key: "value"}
+      expect(described_class.symbolize_keys(hash)).to eq(expected_hash)
+    end
+  end
 end
