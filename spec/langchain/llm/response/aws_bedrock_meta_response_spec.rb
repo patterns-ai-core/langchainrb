eaea9832d092802f5b74734668188994f3637a3b
Kirill Shirinkin <kirill@hey.com>
2024-09-06 18:25:11 +0200

    Add Aws Bedrock Meta support

---
+++ spec/langchain/llm/response/aws_bedrock_meta_response_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+RSpec.describe Langchain::LLM::AwsBedrockMetaResponse do
+  let(:raw_chat_completions_response) {
+    JSON.parse File.read("spec/fixtures/llm/aws_bedrock_meta/complete.json")
+  }
+
+  subject { described_class.new(raw_chat_completions_response) }
+
+  describe "#complete" do
+    it "returns completion" do
+      expect(subject.generation).to eq("The sky has no definitive")
+    end
+  end
+end
