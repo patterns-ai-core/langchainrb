1a62a2d1d31a76e757f4b40389d34b6fb911cf26
Andrei Bondarev <andrei@sourcelabs.io>
2024-09-23 22:42:14 -0400

    Update lib/langchain/llm/openai.rb
    Co-authored-by: Valentino Stoll <codenamev@users.noreply.github.com>

--- lib/langchain/llm/openai.rb
+++ lib/langchain/llm/openai.rb
@@ -45,7 +45,7 @@ module Langchain::LLM
         n: {default: @defaults[:n]},
         temperature: {default: @defaults[:temperature]},
         user: {},
-        response_format: {default: @defaults[:response_format]}
+        response_format: {}
       )
       chat_parameters.ignore(:top_k)
     end
