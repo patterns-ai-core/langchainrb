966eaa80022acca33814a1da8b102d4c64d2e650
Khai Le <khaile.to@gmail.com>
2024-10-03 22:20:03 +0700

    Move adapter-related classes to separate files (#807)
    * Move adapter-related classes to separate files
    
    * Remove AwsBedrock in the Adapter
    
    * Remove all adapters in `Langchain::Assistant` class
    
    * Remove `assistants/llm/adapters/aws_bedrock.rb`
    
    * Rename _base.rb to base.rb
    
    * Update assistant.rb
    
    * Rename open_ai.rb to openai.rb
    
    * Rename base.rb to _base.rb
    
    ---------
    
    Co-authored-by: Andrei Bondarev <andrei@sourcelabs.io>

---
+++ lib/langchain/assistants/llm/adapter.rb
@@ -0,0 +1,27 @@
+Dir[Pathname.new(__FILE__).dirname.join("adapters", "*.rb")].sort.each { |file| require file }
+
+module Langchain
+  class Assistant
+    module LLM
+      # TODO: Fix the message truncation when context window is exceeded
+      class Adapter
+        def self.build(llm)
+          case llm
+          when Langchain::LLM::Anthropic
+            LLM::Adapters::Anthropic.new
+          when Langchain::LLM::GoogleGemini, Langchain::LLM::GoogleVertexAI
+            LLM::Adapters::GoogleGemini.new
+          when Langchain::LLM::MistralAI
+            LLM::Adapters::MistralAI.new
+          when Langchain::LLM::Ollama
+            LLM::Adapters::Ollama.new
+          when Langchain::LLM::OpenAI
+            LLM::Adapters::OpenAI.new
+          else
+            raise ArgumentError, "Unsupported LLM type: #{llm.class}"
+          end
+        end
+      end
+    end
+  end
+end
